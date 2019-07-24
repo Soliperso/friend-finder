ec98401c6b04ae8d4acbe97ac99aa29aa562acd0
Ed Chebli <chah762002@yahoo.fr>
2019-07-23 19:52:55 -0700

    Added index and survey page

---
+++ app/data/friends.js
@@ -0,0 +1,99 @@
+var friendList = [
+  {
+    name: 'Madonna',
+    profilePic: 'https://66.media.tumblr.com/b6e291e72eee7fb75f18100614ab69d4/tumblr_ppg08biJv11ubx89jo1_1280.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: 'Morgan Freeman',
+    profilePic: 'https://66.media.tumblr.com/tumblr_l7v41pjxDA1qb2wjzo1_500.jpg',
+    scores: [
+      "1",
+      "5",
+      "5",
+      "3",
+      "4",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: 'Syvester Stallone',
+    profilePic: 'http://img.timeinc.net/time/photoessays/2010/top10_geezers/sylvester_stallone.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "5",
+      "1",
+      "3",
+      "4",
+      "4",
+      "2",
+      "4"
+    ]
+  },
+  {
+    name: 'Jackie Chan',
+    profilePic: 'https://pbs.twimg.com/media/DKk6nGXXUAEYAtj.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },
+  {
+    name: 'Nicolas Cage',
+    profilePic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNWi0mbMcjwVDhbbG7JO2CDpdh_A7U6v6rs3786ZvubzizQbRV',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  },{
+    name: 'Byonce',
+    profilePic: 'https://66.media.tumblr.com/3b306244baadbf40d7e6c3dbe4a36683/tumblr_pn08so7ghU1vt10pl_1280.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
